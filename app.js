af809a5612e38e528134284900dc72ee1981ea45
tanuraj panwar <tanurajpanwar@gmail.com>
2024-07-20 18:23:08 +0530

    episode-1

---
+++ app.js
@@ -0,0 +1,5 @@
+let heading = React.createElement("h1",{},"Hello From React");
+console.log(heading)
+let root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root)
+root.render(heading);
